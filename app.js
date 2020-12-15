fe306f8092e672a5dafbe64cf51bcf2995d0be1b
Victor <victorbultez2@gmail.com>
2020-12-15 19:37:58 +0100

    Ajout dossier pour un jeu prochain

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const client = new Discord.Client({disableEveryone: true});
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -9,11 +9,13 @@ client.on('message', msg => {
   if (msg.content === 'sale') {
     msg.channel.send('p***!');
   }
-});
-
-client.on('message', msg => {
   if (msg.content === 'wsh') {
     msg.reply('mon sauce!');
+  }
+  if(msg.content === 'jouer'){
+    msg.channel.send(msg.member.nickname + ' est en train de jouer');
+    //let game = new game(msg.member.nickname);
+    //game.showPlayer;
   }
 });
 
---
+++ game/game.js
@@ -0,0 +1,26 @@
+class game{
+  constructor(player){
+    this.player = player;
+  }
+
+  startGame(){
+    //todo...
+  }
+
+  endGame(){
+    //todo...
+  }
+
+  showPlayer(){
+    console.log(this.player);
+  }
+
+}
+
+module.exports = {
+
+  showPlayer(){
+    console.log(this.player);
+  }
+
+}
