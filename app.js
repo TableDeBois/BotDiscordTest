c37264610cdaf1810b7ac3074058b7071dd60cc0
Victor <victorbultez2@gmail.com>
2020-12-15 18:40:14 +0100

    bot's first messages

---
+++ app.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'sale') {
+    msg.channel.send('p***!');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'wsh') {
+    msg.reply('mon sauce!');
+  }
+});
+
+
+client.login('token');
